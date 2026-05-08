9778a1d4e77f7abc67f65e3d7a5158d84217e273
Andrey Litvitski <andrey1010102008@gmail.com>
2026-05-08 22:40:22 +0300

    support java 25

---
+++ packages/java/25.0.0/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+: "${JAVA_TARBALL_URL:=https://api.adoptium.net/v3/binary/latest/25/ga/linux/x64/jdk/hotspot/normal/eclipse}"
+
+curl -L "$JAVA_TARBALL_URL" -o java.tar.gz
+tar xzf java.tar.gz --strip-components=1
+rm java.tar.gz
+
