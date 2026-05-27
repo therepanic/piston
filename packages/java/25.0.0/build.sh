9ba322ef553d76aabc512dbda9a381785188e9c2
Andrey Litvitski <andrey1010102008@gmail.com>
2026-05-27 20:48:29 +0300

    maybe gson

--- packages/java/25.0.0/build.sh
+++ packages/java/25.0.0/build.sh
@@ -2,8 +2,13 @@
 set -euo pipefail
 
 : "${JAVA_TARBALL_URL:=https://api.adoptium.net/v3/binary/latest/25/ga/linux/x64/jdk/hotspot/normal/eclipse}"
+: "${GSON_VERSION:=2.13.1}"
 
 curl -L "$JAVA_TARBALL_URL" -o java.tar.gz
 tar xzf java.tar.gz --strip-components=1
 rm java.tar.gz
+
+mkdir -p libs
+curl -L "https://repo1.maven.org/maven2/com/google/code/gson/gson/${GSON_VERSION}/gson-${GSON_VERSION}.jar" \
+  -o "libs/gson-${GSON_VERSION}.jar"
 
